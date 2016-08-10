66ef5e5bac582be20406fe7b849c93ccfa51583e
Sleepwalker <Nico_Gabin@yahoo.com>
2016-08-09 21:20:04 -0300

    Especie de confirmacion
    Casi le puse ganas a hacer la confirmación de usuario y contraseña pero
    me las arreglé para cagarla más, ni me pregunten

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -7,25 +7,30 @@ import javax.swing.border.EmptyBorder;
 
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JSpinner;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import java.util.ArrayList;
-
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
-	
+	public int i, errorTotal = 0;
+	String User[] = { "Admin", "Vendedor" };
+	String Password[] = { "rootlindo", "12345" };
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		String User[] = {"Admin", "Vendedor"};
-		String Password[] = {"rootlindo", "12345"};
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -55,6 +60,37 @@ public class VentanaPrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	}
 
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Usuario = String.valueOf(JOptionPane.showInputDialog("Ingrese nombre de usuario"));
+				String Contraseña = String.valueOf(JOptionPane.showInputDialog("Ingrese la contraseña"));
+				for (i = 0; i < 2; i++) {
+					if ((Usuario == User[i]) && (Contraseña == Password[i])) {
+						JOptionPane.showMessageDialog(null, "Congratu-fucking-lations");
+					} else {
+						errorTotal++;
+					}
+					if (errorTotal == 2) {
+						JOptionPane.showMessageDialog(null, "Nombre de usuario o contraseña incorrecta", "ERROR",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+
+			}
+
+		});
+		btnLogin.setBounds(103, 575, 89, 23);
+		contentPane.add(btnLogin);
+
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnExit.setBounds(338, 575, 89, 23);
+		contentPane.add(btnExit);
+	}
 }
