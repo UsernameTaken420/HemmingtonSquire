65c76771b44ce99698b2f428f3afeac31dc1c918
Kulin <nahubtiel@gmail.com>
2016-08-28 13:48:47 -0300

    Logs
    Agregado Jmenu sobre logs

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -28,12 +28,23 @@ import javax.swing.JScrollPane;
 public class VentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
-	private static ArrayList <String> user=new ArrayList <String>();
-	private static ArrayList <String> pass=new ArrayList <String>();
+	private static ArrayList<String> user = new ArrayList<String>();
+	private static ArrayList<String> pass = new ArrayList<String>();
 	public int i, errorTotal = 0;
 	private JTextField textField;
+	private JTextArea textArea_1;
+	private JButton btnNewButton;
+	private JMenu mnArchivo;
+	private JMenuItem mntmConfirmarCambios;
+	private JMenuItem mntmConfirmarCambiosY;
+	private JMenuItem mntmNewMenuItem;
+	private JMenu mnUsuarios;
+	private JMenuItem mntmAgregar;
+	private JMenuItem mntmEliminar;
+	private JMenu mnMercaderia;
+	private JMenuItem mntmEliminar_1;
+	private JMenuItem mntmAgregar_1;
 
-	
 	/**
 	 * Launch the application.
 	 */
@@ -61,65 +72,100 @@ public class VentanaPrincipal extends JFrame {
 				System.exit(0);
 			}
 		});
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(50, 10, 492, 493);
+		setBounds(50, 10, 620, 380);
+		
 		JMenuBar menuBar = new JMenuBar();
-		 setJMenuBar(menuBar);
-		 JMenu mnArchivo = new JMenu("Archivo");
-		 menuBar.add(mnArchivo);
-		 JMenuItem mntmConfirmarCambios = new JMenuItem("Confirmar cambios");
-		 mnArchivo.add(mntmConfirmarCambios);
-		 JMenuItem mntmConfirmarCambiosY = new JMenuItem("Confirmar cambios y salir");
-		 mnArchivo.add(mntmConfirmarCambiosY);
-		 JMenuItem mntmNewMenuItem = new JMenuItem("Salir");
-		 mnArchivo.add(mntmNewMenuItem);
-		 JMenu mnUsuarios = new JMenu("Usuarios");
-		 menuBar.add(mnUsuarios);	
-		 JMenuItem mntmAgregar = new JMenuItem("Agregar");
-		 mnUsuarios.add(mntmAgregar);
-		 JMenuItem mntmEliminar = new JMenuItem("Eliminar");
-		 mnUsuarios.add(mntmEliminar);
-		 JMenu mnMercaderia = new JMenu("Mercaderia");
-		 menuBar.add(mnMercaderia);
-		 JMenuItem mntmAgregar_1 = new JMenuItem("Agregar");
-		 mnMercaderia.add(mntmAgregar_1);	
-		 JMenuItem mntmEliminar_1 = new JMenuItem("Eliminar");
-		 mnMercaderia.add(mntmEliminar_1);
+		setJMenuBar(menuBar);
+		
+		mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		mntmConfirmarCambios = new JMenuItem("Confirmar cambios");
+		mnArchivo.add(mntmConfirmarCambios);
+		
+		mntmConfirmarCambiosY = new JMenuItem("Confirmar cambios y salir");
+		mnArchivo.add(mntmConfirmarCambiosY);
+		
+		mntmNewMenuItem = new JMenuItem("Salir");
+		mnArchivo.add(mntmNewMenuItem);
+		
+		mnUsuarios = new JMenu("Usuarios");
+		menuBar.add(mnUsuarios);
+		
+		mntmAgregar = new JMenuItem("Agregar");
+		mnUsuarios.add(mntmAgregar);
+		
+		mntmEliminar = new JMenuItem("Eliminar");
+		mnUsuarios.add(mntmEliminar);
+		
+		mnMercaderia = new JMenu("Mercaderia");
+		menuBar.add(mnMercaderia);
+		
+		mntmAgregar_1 = new JMenuItem("Agregar");
+		mnMercaderia.add(mntmAgregar_1);
+		
+		mntmEliminar_1 = new JMenuItem("Eliminar");
+		mnMercaderia.add(mntmEliminar_1);
+		
+		JMenuItem mntmModificarIva = new JMenuItem("Modificar IVA");
+		mnMercaderia.add(mntmModificarIva);
+		
+		JMenuItem mntmActualizarMercaderia = new JMenuItem("Actualizar mercaderia");
+		mnMercaderia.add(mntmActualizarMercaderia);
+		
+		JMenu mnLogs = new JMenu("Logs");
+		menuBar.add(mnLogs);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Movimientos");
+		mnLogs.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmVentas = new JMenuItem("Ventas");
+		mnLogs.add(mntmVentas);
+		
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.LIGHT_GRAY);
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		user.add("Admin");
 		pass.add("rootlindo");
-		Login_Frame Login = new Login_Frame();
 		
+		Login_Frame Login = new Login_Frame();
 		contentPane.setLayout(null);
-		 textField = new JTextField();
-		 textField.setBounds(10, 41, 108, 20);
-		 contentPane.add(textField);
-		 textField.setColumns(10);
-		 JLabel lblCodigoDeProducto = new JLabel("Codigo de producto");
-		 lblCodigoDeProducto.setBounds(10, 11, 139, 26);
-		 lblCodigoDeProducto.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		 contentPane.add(lblCodigoDeProducto);
-		 JButton btnNewButton = new JButton("Buscar");
-		 btnNewButton.setBounds(128, 40, 67, 23);
-		 contentPane.add(btnNewButton);
-		 JScrollPane scrollPane = new JScrollPane();
-		 scrollPane.setBounds(10, 72, 185, 290);
-		 contentPane.add(scrollPane);
-		 JTextArea textArea = new JTextArea();
-		 scrollPane.setViewportView(textArea);
-		 JLabel lblImagen = new JLabel("Imagen");
-		 lblImagen.setBounds(205, 139, 203, 201);
-		 contentPane.add(lblImagen);
-		 JLabel lblDescripcion = new JLabel("Descripcion");
-		 lblDescripcion.setBounds(209, 11, 139, 26);
-		 lblDescripcion.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		 contentPane.add(lblDescripcion);
-		 
-		 JTextArea textArea_1 = new JTextArea();
-		 textArea_1.setBounds(205, 48, 203, 82);
-		 contentPane.add(textArea_1);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 41, 108, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblCodigoDeProducto = new JLabel("Codigo de producto");
+		lblCodigoDeProducto.setBounds(10, 11, 139, 26);
+		
+		lblCodigoDeProducto.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblCodigoDeProducto);
+		
+		btnNewButton = new JButton("Buscar");
+		btnNewButton.setBounds(128, 40, 67, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblImagen = new JLabel("Imagen");
+		lblImagen.setBounds(366, 56, 139, 119);
+		contentPane.add(lblImagen);
+		
+		JLabel lblDescripcion = new JLabel("Descripcion");
+		lblDescripcion.setBounds(366, 11, 139, 26);
+		
+		lblDescripcion.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblDescripcion);
+
+		textArea_1 = new JTextArea();
+		textArea_1.setBounds(366, 186, 203, 82);
+		contentPane.add(textArea_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 87, 319, 188);
+		contentPane.add(textArea);
 	}
 }
